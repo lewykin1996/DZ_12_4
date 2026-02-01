02a22b92f02b7c2dd9a4b3aa548509105f26b75b
lewykin1996 <lewykin1996@gmail.com>
2026-02-01 12:38:46 +0300

    Add files via upload

---
+++ Считывание двумерного массива их файла.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	//setlocale(LC_ALL, "Russian");
+	//SetConsoleCP(1251);
+	//SetConsoleOutputCP(1251);
+
+	std::ifstream ifile{ "in.txt" };
+
+	if (!ifile)
+	{
+		cout << "Ошибка открытия файла\n";
+		return 1;
+	}
+
+	int a, b;
+	ifile >> a >> b;
+
+	int** arr = new int*[a];
+
+	// создаем двумерный массив
+	for (int i = 0; i < a; i++)
+	{
+		arr[i] = new int[b];
+	}
+
+	// заполняем массив из файла
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+		{
+			ifile >> arr[i][j];
+		}
+	}
+
+	// выводим с разворотом каждой строки
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = b - 1; j >= 0; j--)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < a; i++) // освобождаем память
+	{
+		delete[] arr[i];
+	}
+	delete[] arr;
+
+	ifile.close();
+
+	return 0;
+}
